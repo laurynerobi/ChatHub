b2e28cee5adaeafd9e44f9b73bae6882ca7f5607
laurynerobi <71785767+laurynerobi@users.noreply.github.com>
2023-10-19 15:03:07 -0400

    added user commands

--- src/Client.java
+++ src/Client.java
@@ -2,8 +2,11 @@ package src;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class Client implements Runnable {
+
+
     private Socket myClient;
     private BufferedReader reader;
     private PrintWriter writer;
@@ -15,10 +18,9 @@ public class Client implements Runnable {
             reader = new BufferedReader(new InputStreamReader(myClient.getInputStream()));
             writer = new PrintWriter(myClient.getOutputStream(), true);
 
-            //creating a thread for the clientInputHandler
             clientInputHandler myHandler = new clientInputHandler();
             Thread thread = new Thread(myHandler);
-            thread.start(); //to open a separate thread
+            thread.start();
 
             String clientText;
             while ((clientText = reader.readLine()) !=null){
@@ -41,32 +43,42 @@ public class Client implements Runnable {
             //IGNORE
         }
     }
+
     class clientInputHandler implements Runnable{
 
+        private String username;
         @Override
         public void run() {
             try{
-                //passing command line input
                 BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+
                 while(!complete) {
                     String text = inputReader.readLine();
                     if(text.equals("/quit")) {
                         writer.println("has left the chat");
                         inputReader.close();
                         shutdown();
-                    }
-                    //handling file sending and receiving
-                    else if (text.startsWith("/sendfile ")){
+                    } else if (text.startsWith("/sendfile ")){
                         String[] parts = text.split(" ");
                         if (parts.length == 3) {
                             String receiverUsername = parts[1];
                             String filename = parts[2];
                             sendFile(receiverUsername, filename);
-                        }else {
+                        } else {
                             System.out.println("Invalid file send command. Use /sendfile <username> <filename>");
-
                         }
-                    } else { //sends text to the server
+                    }
+//                    else if(text.startsWith("/user ")) {
+//                        String[] divideMessage = text.split(" ", 2);
+//                        if (divideMessage.length == 2) {
+//                            String oldUsername = username;
+//                            username = divideMessage[1];
+//                            writer.println(oldUsername + " changed their username to " + username);
+//                        } else {
+//                            writer.println("Alert: No username was provided!");
+//                        }
+//                    }
+                    else {
                         writer.println(text);
                     }
                 }
@@ -75,43 +87,30 @@ public class Client implements Runnable {
             }
         }
 
-        //file handling
         public void sendFile(String receiverUsername, String filename) {
             try {
                 File file = new File(filename);
-                byte[] fileData = new byte[(int) file.length()];
-                FileInputStream fis = new FileInputStream(file);
-                BufferedInputStream bis = new BufferedInputStream(fis);
-                bis.read(fileData, 0, fileData.length);
-
-                writer.println("/file " + receiverUsername + " " + filename);
-                writer.println(fileData.length); // Send the file size
-                writer.flush();
-                writer.write(String.valueOf(fileData), 0, fileData.length);
-                writer.flush();
+                if (file.exists()) {
+                    byte[] data = Files.readAllBytes(file.toPath());
+                    writer.println("/sendfile " + receiverUsername + " " + filename);
+                    for (byte b : data) {
+                        writer.print((char)b);
+                    }
+                    writer.println(); // End of file
+                    writer.flush();
+                } else {
+                    System.out.println("The specified file does not exist!");
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-        public void receiveFile(String senderUsername, String filename, byte[] data) {
-            try {
-                // Implement code to save the received file
-                // For example, save it to a user-specific directory
-                File receivedFile = new File("received_files/" + filename);
-                FileOutputStream fos = new FileOutputStream(receivedFile);
-                fos.write(data);
-                fos.close();
-                System.out.println("File received from " + senderUsername + ": " + filename);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
     }
 
     public static void main(String[] args) {
-        Client client = new Client();
-        client.run();
+        Client myClient = new Client();
+        myClient.run();
     }
+
 }
+
--- src/Server.java
+++ src/Server.java
@@ -3,13 +3,12 @@ package src;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-//implements runnable interface
-//runnable causing server to be passed to thread
 public class Server implements Runnable {
     private ArrayList<ClientConnectHandler> connectedClients;
     private ServerSocket myServer;
@@ -24,14 +23,10 @@ public class Server implements Runnable {
     @Override
     public void run() {
         try {
-            //pass the port number only
             myServer = new ServerSocket(7000);
             pool = Executors.newCachedThreadPool();
             while(!complete) {
-
-                //when connection accepted socket created.
                 Socket client = myServer.accept();
-                //holds all the connected users/clients
                 ClientConnectHandler myHandler = new ClientConnectHandler(client);
                 connectedClients.add(myHandler);
                 pool.execute(myHandler);
@@ -41,16 +36,15 @@ public class Server implements Runnable {
             exitHandler();
         }
     }
-    //sending text to all connected clients
+
     public void messenger(String text){
         for (ClientConnectHandler connect : connectedClients) {
-           if(connect != null){
-               connect.sendText(text);
-           }
+            if(connect != null){
+                connect.sendText(text);
+            }
         }
     }
 
-    //to handle termination of the server
     public void exitHandler () {
         try {
             complete = true;
@@ -62,12 +56,11 @@ public class Server implements Runnable {
                 connect.exitHandler();
             }
         }catch(IOException exception) {
-                //ignore
-            }
+            //ignore
         }
-    //handles each client connection
+    }
+
     class ClientConnectHandler implements Runnable{
-        //instance of clientConnections to handle multiple clients concurrently
         private Socket myClient;
         private BufferedReader reader;
         private PrintWriter writer;
@@ -88,42 +81,55 @@ public class Server implements Runnable {
         @Override
         public void run() {
             try {
-                //deal with client
-                //initialize reader and writer
                 reader = new BufferedReader(new InputStreamReader(myClient.getInputStream()));
                 writer = new PrintWriter(myClient.getOutputStream(),true);
-                //prompt user for their username
-                writer.println("Welcome to CHATHUB! Your number one chatting service.");
+                writer.println("Welcome to CHATHUB! Your number one communication service.");
+                writer.println(" ");
+                writer.println("To change your username , type: /user");
+                writer.println("To exit the app, type: /quit");
+                writer.println(" ");
                 writer.println("Please enter your username: ");
                 username = reader.readLine();
-                //checks if username is blank
-//                if(username.isBlank()){
-//                    writer.println("Enter a valid user name");
-//                    username = reader.readLine();
-//                }
                 System.out.println(username + " is connected");
                 messenger(username + " joined the party!");
 
-                //loop to always ask for new messages
                 String clientMessage;
                 while ((clientMessage = reader.readLine()) != null) {
-                    if(clientMessage.startsWith("/user ")) {
-                        //handle username
-                        String[] divideMessage = clientMessage.split(" ", 2);
-                        if (divideMessage.length == 2) {
-                            messenger(username + "changed their username to " + divideMessage[1]);
-                            System.out.println(username + "changed their username to " + divideMessage[1]);
-                            username = divideMessage[1];
-                            writer.println("Username changed to " + username);
-                        } else {
-                            writer.println("Alert: No username was provided!");
+                    if(clientMessage.startsWith("/sendfile ")) {
+                        String[] divideMessage = clientMessage.split(" ", 4);
+                        if (divideMessage.length == 4) {
+                            String receiverUsername = divideMessage[1];
+                            String filename = divideMessage[2];
+                            File file = new File(filename);
+                            if (file.exists()) {
+                                byte[] data = Files.readAllBytes(file.toPath());
+                                forwardFile(username, receiverUsername, filename, data);
+                            } else {
+                                writer.println("Alert: The specified file does not exist!");
+                            }
                         }
-                    }   else if (clientMessage.startsWith("/quit ")) {
+//                        } else {
+//                            writer.println("Alert: Invalid command format! Use /sendfile receiverUsername filename");
+//                        }
+                    } else if (clientMessage.startsWith("/quit ")) {
                         messenger(username + " has left the chat!");
                         writer.flush();
                         exitHandler();
-                    }else {
-                        // if no command send username and message to all other users in the chat
+                    } else if (clientMessage.startsWith("/user ")) {
+                        {
+                            //handle username
+                            String[] divideMessage = clientMessage.split(" ", 2);
+                            if (divideMessage.length == 2) {
+                                messenger(username + " changed their username to " + divideMessage[1]);
+                                System.out.println(username + " changed their username to " + divideMessage[1]);
+                                username = divideMessage[1];
+                                writer.println("Username changed to " + username);
+                            } else {
+                                writer.println("Alert: No username was provided!");
+                            }
+                        }
+                        
+                    } else {
                         messenger(username + ": " + clientMessage);
                     }
                 }
@@ -132,6 +138,7 @@ public class Server implements Runnable {
                 exitHandler();
             }
         }
+
         public void sendText (String text) {
             writer.println(text);
         }
@@ -140,19 +147,15 @@ public class Server implements Runnable {
             try {
                 reader.close();
                 writer.close();
-            if (!myClient.isClosed()) {
-                myClient.close();
-            }
+                if (!myClient.isClosed()) {
+                    myClient.close();
+                }
             } catch(IOException exception) {
                 //ignore
-                }
             }
+        }
 
-        private ConcurrentHashMap<ClientConnectHandler, PrintWriter> connectedClientsWithWriters = new ConcurrentHashMap<>();
-
-        //file handling
         public void forwardFile(String senderUsername, String receiverUsername, String filename, byte[] data) {
-            // Check if the receiver is connected
             for (ClientConnectHandler client : connectedClients) {
                 if (client.getUsername().equals(receiverUsername)) {
                     client.receiveFile(senderUsername, filename, data);
@@ -163,16 +166,13 @@ public class Server implements Runnable {
 
         public void receiveFile(String senderUsername, String filename, byte[] data) {
             try {
-                // Create a directory for received files (if it doesn't exist)
                 File receivedDir = new File("received_files");
                 if (!receivedDir.exists()) {
                     receivedDir.mkdir();
                 }
 
-                // Create a file in the received_files directory
                 File receivedFile = new File("received_files", filename);
 
-                // Write the received data to the file
                 FileOutputStream fos = new FileOutputStream(receivedFile);
                 fos.write(data);
                 fos.close();
@@ -182,16 +182,12 @@ public class Server implements Runnable {
                 e.printStackTrace();
             }
         }
-
-
-
     }
 
 
-        public static void main(String[] args) {
-    Server myServer = new Server();
-    myServer.run();
+    public static void main(String[] args) {
+        Server myServer = new Server();
+        myServer.run();
     }
 
 }
-
